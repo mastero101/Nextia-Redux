ecbc9baa222f073a54c003fe31b50e2b5e614656
mastero <castro.alejandro17@gmail.com>
2023-01-10 01:47:21 -0600

    images size correct on benevits

--- src/app/benevit/benevit.page.ts
+++ src/app/benevit/benevit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-benevit',
@@ -11,5 +12,4 @@ export class BenevitPage implements OnInit {
 
   ngOnInit() {
   }
-
 }
